61def167dde153bcee8b93c31b0cf8e7640e74cc
fdetsch <florian.detsch@staff.uni-marburg.de>
2016-10-10 17:20:23 +0200

    Update: added TODO file to .gitignore; visualization of MODIS fire cells for presentation.

--- R/displayData.R
+++ R/displayData.R
@@ -22,6 +22,20 @@ rst_agg <- overlay(rst_yrs, fun = function(...) max(..., na.rm = TRUE))
 m1 <- mapview(rst_agg, col.regions = envinmrPalette(length(unique(rst_agg[])) - 1), 
               at = seq(1999.5, 2016.5, 1), map.types = "OpenTopoMap")
 
+rgb <- kiliAerial(template = rst_agg, projection = "+init=epsg:4326")
+tmp <- rst_agg; tmp[tmp[] == 0] <- NA
+spy_agg <- rasterToPolygons(rst_agg)
+p1 <- spplot(spy_agg, col = NA, alpha.regions = .6, scales = list(draw = TRUE), 
+             sp.layout = rgb2spLayout(rgb, quantiles = c(0, 1), alpha = .6), 
+             at = seq(1999.5, 2016.5, 1), main = "Year of Last Fire Incident")
+
+library(grid)
+tiff("../../work/projects/bale/events/workshop_oct2016/presentations/subproject/img/fires_modis.tiff", 
+     width = 20, height = 18, units = "cm", res = 300, compression = "lzw")
+grid.newpage()
+print(p1, newpage = FALSE)
+dev.off()
+
 
 ### highlight recurrent fires -----
 
