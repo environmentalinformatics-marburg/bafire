61224228a4c3da4285ae9c2caf710a1398b95d49
fdetsch <florian.detsch@staff.uni-marburg.de>
2017-03-02 20:50:26 +0100

    Renamed script.

--- R/rapid_rad.R
+++ R/rapid_rad.R
@@ -1,29 +1,30 @@
-#' RapidEye Top-of-Atmosphere Radiances
+#' RapidEye At-Sensor Radiances
 #' 
 #' @description 
-#' Create RapidEye top-of-Atmosphere Radiances (ToAR) from raw digital numbers 
-#' (DN).
+#' Create RapidEye at-sensor radiances from raw digital numbers (DN).
 #' 
-#' @param x RapidEye band(s) as \code{Raster*} or \code{character}. The number 
-#' and order of bands must match with the scale factors in 'meta'.
+#' @param x RapidEye band(s) with raw DN values as \code{Raster*} or 
+#' \code{character}. The number and order of bands must match with the scale 
+#' factors in 'meta'.
 #' @param meta Associated metadata as \code{character} or \code{xml*} (see 
 #' \code{\link{rapid_sf}}).
 #' @param digits Number of decimal places passed to \code{\link{round}}. Values  
-#' other than \code{NULL} enable rounding which might lead to reduced memory 
-#' usage through an adapting \code{Raster*} \code{\link{dataType}}.
+#' other than \code{NULL} (default) enable rounding which likely results in 
+#' reduced memory usage.
 #' @param ... Additional arguments passed to \code{\link{writeRaster}}.
 #' 
 #' @return 
-#' A ToAR \code{Raster*}.
+#' A \code{Raster*} with at-sensor radiances in watts per steradian per square 
+#' meter.
 #' 
 #' @author 
 #' Florian Detsch
 #' 
 #' @seealso \code{\link{rapid_sf}}
 #' 
-#' @export rapid_toar
-#' @name rapid_toar
-rapid_toar <- function(x, meta, digits = NULL, ...) {
+#' @export rapid_rad
+#' @name rapid_rad
+rapid_rad <- function(x, meta, digits = NULL, ...) {
   
   ## if required, import 'character' input
   if (inherits(x, "character")) x <- raster::brick(x)
@@ -32,7 +33,7 @@ rapid_toar <- function(x, meta, digits = NULL, ...) {
   ## extract scale factors from metadata  
   scf <- rapid_sf(meta)
   
-  ## calculate toar from raw dn and layer-specific scale factors
+  ## calculate at-sensor radiances from raw dn and layer-specific scale factors
   raster::calc(x, fun = function(x) {
     val <- x * scf
     if (!is.null(digits)) val <- round(val, digits)
