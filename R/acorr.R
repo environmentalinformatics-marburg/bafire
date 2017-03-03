f56c0e0e5c4d25dd00ed040fd810cc045ce1b7ee
Florian Detsch <fdetsch@sd19006.Geographie.Uni-Marburg.DE>
2017-03-03 13:34:37 +0100

    Update: ensured compatibility with server.

--- R/acorr.R
+++ R/acorr.R
@@ -4,21 +4,20 @@
 rm(list = ls(all = TRUE))
 
 ## working directory
-setwd("/media/permanent/data/bale")
+setwd("/media/memory01/data/casestudies/bale")
 
 ## packages
 # devtools::install_github("environmentalinformatics-marburg/reset")
-library(reset)
 # devtools::install_github("italocegatta/rapidr")
-library(rapidr)
-library(rgrass7)
 # devtools::install_github("fdetsch/Orcs")
-library(Orcs)
+lib <- c("parallel", "reset", "rapidr", "rgrass7", "Orcs")
+Orcs::loadPkgs(lib)
 
 ## functions
-source("../../repo/bafire/R/rapid_qc.R")
-source("../../repo/bafire/R/rapid_rad.R")
-source("../../repo/bafire/R/rapid_ref.R")
+bfr <- "../../users/fdetsch/repo/bafire/"
+jnk <- sapply(c("getInfo", "rapid_qc", "rapid_rad", "rapid_ref"), function(i) {
+  source(paste0(bfr, "R/", i, ".R"))
+})
 
 ## raster options, write format used for temporary files and readable from GRASS
 jnk <- capture.output(opt <- rasterOptions())
@@ -27,67 +26,31 @@ rasterOptions(format = "HFA")
 ## grass-gis initialization parameters
 os <- Sys.info()[["sysname"]]
 gisBase <- system("grass72 --config path", intern = TRUE) # works on windows?
-gisDbase <- paste0(switch(os, "Linux" = "/media/permanent/", "Windows" = "E:/"), 
-                   "data/grassdata/")
+gisDbase <- "../../grassdata/"
 
 ## parallelization
-library(parallel)
-cl <- makePSOCKcluster(3L)
+cl <- makePSOCKcluster(detectCores() * .75)
+clusterExport(cl, "bfr")
+jnk <- clusterEvalQ(cl, source(paste0(bfr, "R/", i, ".R")))
 
 
-### quality control -----
+### extraction -----
 
-drs <- "rapideye/3742218_2016-02-11_RE2_3A_627058"
-fls <- list.files(drs, pattern = "_3A_", full.names = TRUE)
+## list available file containers
+ctn <- list.files("rapideye", pattern = "^getProduct.*streaming=True$",
+                  full.names = TRUE)
 
-## rapideye bands
-tfs <- fls[grep(".tif$", fls)]
-bds <- brick(tfs[2])
-
-## unusable data mask (udm)
-qty <- raster(tfs[3])
-
-## perform quality control
-mat <- matrix(rep(1, 25), ncol = 5); mat[3, 3] <- 0
-qcl <- rapid_qc(bds, qty, directions = mat)
-
-
-### top-of-atmosphere (toa) reflectances -----
-
-## metadata
-mtd <- fls[grep(".xml$", fls)]
-mtd <- xml2::read_xml(mtd)
-
-## compute toa reflectances
-rfl <- rapid_ref(qcl, mtd, digits = 3L)
-rm(qcl)
+## loop over containers and extract required files
+lst <- parLapply(cl, ctn, function(i) {
+  fls <- unzip(i, list = TRUE)$Name
+  ord <- unique(getOrder(fls))
+  ids <- sapply(c(paste0(c(ord, "udm"), ".tif$"), "metadata.xml"), 
+                function(j) grep(j, fls))
+  unzip(i, files = fls[ids], exdir = dirname(i), overwrite = FALSE)
+})
 
 
-### atmospheric correction (see 
-### https://grasswiki.osgeo.org/wiki/Atmospheric_correction) -----
-
-## geometrical conditions
-re_dt <- rapid_date(mtd); re_hr <- rapid_hour(mtd); re_ll <- rapid_latlon(mtd)
-
-geo <- paste(format(re_dt, "%m %d")                         # month and day
-             , round(re_hr, 2)                              # decimal hours
-             , paste(rev(round(re_ll, 3)), collapse = " ")) # coordinates
-
-## mean elevation (tile-based information could be stored in separate images)
-dem_utm <- raster("dem/dem_srtm_01_utm.tif")
-dem_res <- resample(dem_utm, bds)
-
-ele <- round(mean(dem_res[]) / 1000 * (-1), digits = 3)
-
-## aerosol optical depth (aod)
-hdf <- list.files("modis", pattern = ".hdf$", full.names = TRUE)
-hdf <- hdf[grep(format(re_dt, "%Y%j"), hdf)]
-
-aod <- do.call(mean, lapply(hdf, function(fl) {
-  sds <- getSwathSDS(fl, "Aerosol_Optical_Depth_Land_Ocean_Mean", 
-                     ext = projectExtent(bds, crs = "+init=epsg:4326"))
-  round(mean(sds[[1]][[1]][], na.rm = TRUE), digits = 3)
-}))
+### initialize grass session -----
 
 ## initialize new location and baseline mapset with required epsg code
 initGRASS(gisBase = gisBase, home = raster::tmpDir(), 
@@ -96,56 +59,117 @@ initGRASS(gisBase = gisBase, home = raster::tmpDir(),
 
 execGRASS("g.proj", flags = "c", epsg = 32637)
 
-## loop over single bands
-atc <- vector("list", nlayers(rfl))
-for (i in 1:nlayers(rfl)) {
+
+### quality control -----
+
+out <- vector("list", length(lst)); n <- 1L
+for (h in lst) {
   
-  ## band code
-  bnd <- switch(as.character(i), "1" = 88, "2" = 89, "3" = 90, "4" = 91, "5" = 92)
+  ## status message
+  cat("Image", n, "of", length(lst), paste0("(", pureBasename(h[1]), ")"), 
+      "is in, start processing.\n")
   
-  ## create parameter file
-  prm <- c(13      # sensor, 13 = "rapideye"
-           , geo   # geometrical conditions 
-           , 1     # atmospheric model, 1 = "tropical"
-           , 1     # aerosols model, 1 = "continental"
-           , 0     # visibility, 0 = use aod (next line)
-           , aod   # aod
-           , ele   # mean elevation
-           , -1000 # sensor height, -1000 = sensor onboard a satellite
-           , bnd   # satellite band, 88 (:92) = rapideye blue (to nir) band
-  )
+  ## import rapideye bands, unusable data mask, and metadata (file order in each 
+  ## list entry results from extraction above)
+  bds <- brick(h[1])
+  qty <- raster(h[2])
+  mtd <- xml2::read_xml(h[3])
   
-  fls_prm <- paste0(drs, "/", gsub(".tif$", "", basename(tfs[2])), "_param.txt")
-  writeLines(prm, fls_prm)
+  ## perform quality control with 2-pixel buffer around each unusable cell
+  mat <- matrix(rep(1, 25), ncol = 5); mat[3, 3] <- 0
+  qcl <- rapid_qc(bds, qty, directions = mat)
   
-  ## during first run, initialize new mapset inheriting projection info
-  if (i == 1) {
-    execGRASS("g.mapset", flags = "c", mapset = pureBasename(tfs[2]))
+  
+  ### top-of-atmosphere (toa) reflectances -----
+  
+  rfl <- rapid_ref(qcl, mtd)
+  rm(qcl)
+  
+  
+  ### atmospheric correction (see 
+  ### https://grasswiki.osgeo.org/wiki/Atmospheric_correction) -----
+  
+  ## geometrical conditions
+  re_dt <- rapid_date(mtd); re_hr <- rapid_hour(mtd); re_ll <- rapid_latlon(mtd)
+  
+  geo <- paste(format(re_dt, "%m %d")                         # month and day
+               , round(re_hr, 2)                              # decimal hours
+               , paste(rev(round(re_ll, 3)), collapse = " ")) # coordinates
+  
+  ## mean elevation (tile-based information could be stored in separate images)
+  dem_utm <- raster("dem/dem_srtm_01_utm.tif")
+  dem_res <- resample(dem_utm, bds)
+  
+  ele <- round(mean(dem_res[]) / 1000 * (-1), digits = 3)
+  
+  ## aerosol optical depth (aod)
+  mod <- list.files("modis", full.names = TRUE, 
+                    pattern = "Aerosol_Optical_Depth.tif$")
+  mod <- mod[grep(format(re_dt, "%Y%j"), mod)]
+  
+  aod <- do.call(mean, lapply(mod, function(fl) {
+    rst <- raster(fl)
+    crp <- crop(rst, projectExtent(bds, crs = "+init=epsg:4326"))
+    round(mean(crp[], na.rm = TRUE), digits = 3)
+  }))
+  
+  ## loop over single bands
+  atc <- vector("list", nlayers(rfl))
+  for (i in 1:nlayers(rfl)) {
     
-    ## assign spatial settings (extent, number of rows and columns, resolution)  
-    ## from resampled digital elevation model to current geographic region
-    execGRASS("r.external", flags = "overwrite",
-              parameters = list(input = attr(dem_res@file, "name"), 
-                                output = "dem"))
+    ## band code
+    bnd <- switch(as.character(i), "1" = 88, "2" = 89, "3" = 90, "4" = 91, "5" = 92)
     
-    execGRASS("g.region", raster = "dem")
+    ## create parameter file
+    prm <- c(13      # sensor, 13 = "rapideye"
+             , geo   # geometrical conditions 
+             , 1     # atmospheric model, 1 = "tropical"
+             , 1     # aerosols model, 1 = "continental"
+             , 0     # visibility, 0 = use aod (next line)
+             , aod   # aod
+             , ele   # mean elevation
+             , -1000 # sensor height, -1000 = sensor onboard a satellite
+             , bnd   # satellite band, 88 (:92) = rapideye blue (to nir) band
+    )
+    
+    fls_prm <- gsub(".tif$", "_param.txt", h[1])
+    writeLines(prm, fls_prm)
+    
+    ## during first run, initialize new mapset inheriting projection info
+    if (i == 1) {
+      execGRASS("g.mapset", flags = "c", mapset = pureBasename(h[1]))
+      
+      ## assign spatial settings (extent, number of rows and columns, resolution)  
+      ## from resampled digital elevation model to current geographic region
+      execGRASS("r.external", flags = "overwrite",
+                parameters = list(input = attr(dem_res@file, "name"), 
+                                  output = "dem"))
+      
+      execGRASS("g.region", raster = "dem")
+    }
+    
+    ## import current rapideye band and perform atmospheric correction
+    execGRASS("r.external", flags = "overwrite", 
+              parameters = list(input = attr(rfl@file, "name"), 
+                                band = i, output = paste0("bnd-", i)))
+    
+    execGRASS("i.atcorr", flags = c("r", "overwrite"), 
+              parameters = list(input = paste0("bnd-", i), elevation = "dem", 
+                                parameters = fls_prm, output = paste0("atc-", i)))
+    jnk <- file.remove(fls_prm)
+    
+    ## write image to disk  
+    atc[[i]] <- raster(readRAST(paste0("atc-", i)))
+    # fls_atc <- gsub(".tif$", paste0("_ATC-", i, ".tif"), h[1])
+    # execGRASS("r.out.gdal", flags = "overwrite", 
+    #           parameters = list(input = "atc", output = fls_atc, 
+    #                             format = "GTiff"))
+    # 
+    # atc[[i]] <- raster(fls_atc)
   }
-
-  ## import current rapideye band and perform atmospheric correction
-  execGRASS("r.external", flags = "overwrite", 
-            parameters = list(input = attr(rfl@file, "name"), 
-                              band = i, output = "bnd"))
-
-  execGRASS("i.atcorr", flags = c("r", "overwrite"), 
-            parameters = list(input = "bnd", elevation = "dem", 
-                              parameters = fls_prm, output = "atc"))
-  file.remove(fls_prm)
-
-  ## write image to disk  
-  fls_atc <- paste0(drs, "/", pureBasename(tfs[2]), "_ATC-", i, ".tif")
-  execGRASS("r.out.gdal", flags = "overwrite", 
-            parameters = list(input = "atc", output = fls_atc, 
-                              format = "GTiff"))
   
-  atc[[i]] <- raster(fls_atc)
+  out[[n]] <- stack(atc); n <- n + 1L
 }
+
+## deregister parallel backend
+stopCluster(cl)
--- R/rapideye.R
+++ R/rapideye.R
@@ -3,75 +3,38 @@
 ## clear workspace
 rm(list = ls(all = TRUE))
 
+## set working directory
+setwd("/media/memory01/data/casestudies/bale")
+
 ## required packages
 # devtools::install_github("MatMatt/MODIS", ref = "develop")
-lib <- c("doParallel", "raster", "rgdal", "MODIS")
+lib <- c("parallel", "raster", "rgdal", "MODIS")
 jnk <- sapply(lib, function(x) library(x, character.only = TRUE))
 
 ## required functions
-source("R/getInfo.R")
+bfr <- "../../users/fdetsch/repo/bafire/"
+source(paste0(bfr, "R/getInfo.R"))
 
 ## parallelization
-cl <- makeCluster(8L)
-registerDoParallel(cl)
+cl <- makePSOCKcluster(8L)
+clusterExport(cl, "bfr")
+jnk <- clusterEvalQ(cl, source(paste0(bfr, "R/getInfo.R")))
 
 
 ### extraction -----
 
 ## list available file containers
-dir_prd <- switch(Sys.info()[["sysname"]], 
-                  "Linux" = "/media/fdetsch/XChange/RapidEye/", 
-                  "Windows" = "D:/RapidEye/")
-
 ctn <- list.files("rapideye", pattern = "^getProduct.*streaming=True$",
                   full.names = TRUE)
 
 ## loop over containers and extract required files
-lst <- foreach(i = ctn) %dopar% {
+lst <- parLapply(cl, ctn, function(i) {
   fls <- unzip(i, list = TRUE)$Name
-  ord <- unique(getOrder(tfs))
+  ord <- unique(getOrder(fls))
   ids <- sapply(c(paste0(c(ord, "udm"), ".tif$"), "metadata.xml"), 
-                function(j) grep(j, tfs))
+                function(j) grep(j, fls))
   unzip(i, files = fls[ids], exdir = dirname(i), overwrite = FALSE)
-}
-
-## re-list extracted files
-drs <- dir(dir_prd, pattern = "_3A_", full.names = TRUE)
-
-lst <- lapply(drs, function(i) {
-  list.files(i, pattern = ".tif$", full.names = TRUE)
 })
-  
-
-### quality control -----
-
-## target folder and files
-dir_qc <- paste0(dir_prd, "qc/")
-if (!dir.exists(dir_qc)) dir.create(dir_qc)
-
-fls_qc <- basename(sapply(lst, "[[", 1))
-fls_qc <- paste0(dir_qc, fls_qc)
-
-lst_qc <- foreach(i = lst, j = as.list(fls_qc), .packages = "raster") %dopar% {
-  # if target file exists, import right away
-  if (file.exists(j)) {
-    stack(j)
-  } else {
-    # else import 5-band raster (optionally apply scale factor of 
-    # 9.999999776482582e-03)
-    rst <- stack(i[1])
-    
-    # import quality layer and resample to 5-m target resolution
-    qc <- raster(i[2])
-    qc <- resample(qc, rst, method = "ngb")
-
-    # discard all pixels with a quality flag value larger than 0    
-    overlay(rst, qc, fun = function(x, y) {
-      x[y[] != 0] <- NA
-      return(x)
-    }, filename = j, format = "GTiff", overwrite = TRUE)
-  }
-}
 
 
 ### -----
