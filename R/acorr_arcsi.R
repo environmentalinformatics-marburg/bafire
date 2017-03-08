f938171d39a54bc23b0d7be145345abdc70518b3
fdetsch <florian.detsch@staff.uni-marburg.de>
2017-03-08 17:01:12 +0100

    Update: added atmospheric water and ozone content to analysis.

--- R/acorr_arcsi.R
+++ R/acorr_arcsi.R
@@ -4,17 +4,17 @@
 rm(list = ls(all = TRUE))
 
 ## working directory
-setwd("~/data/bale")
+setwd("/media/fdetsch/data/bale")
 
 ## packages
 # devtools::install_github("environmentalinformatics-marburg/reset")
 # devtools::install_github("italocegatta/rapidr")
 # devtools::install_github("fdetsch/Orcs")
-lib <- c("parallel", "reset", "rapidr", "Orcs")
+lib <- c("parallel", "rapidr", "Orcs")
 Orcs::loadPkgs(lib)
 
 ## functions
-bfr <- "../../repo/bafire/"
+bfr <- "~/repo/bafire/"
 jnk <- sapply(c("getInfo", "rapid_qc", "rapid_rad", "rapid_ref"), function(i) {
   source(paste0(bfr, "R/", i, ".R"))
 })
@@ -39,8 +39,9 @@ jnk <- sapply(c("getInfo", "rapid_qc", "rapid_rad", "rapid_ref"), function(i) {
 ### atmospheric correction (see 
 ### http://rsgislib.org/arcsi/scripts.html#arcsi-py) -----
 
-## valid arcsi aot values
+## valid arcsi values for aot, water content
 aod_vld <- c(0.0, 0.05, 0.1, 0.15, 0.2, 0.25, 0.3, 0.4, 0.5, 0.75, 0.95)
+wct_vld <- c(0.5, 1:6, 8:9)
 
 ## directions for adjacent pixels
 mat <- matrix(rep(1, 25), ncol = 5); mat[3, 3] <- 0
@@ -51,7 +52,7 @@ mtd <- sapply(drs, function(i) {
   list.files(i, pattern = "metadata.xml$", full.names = TRUE)
 })
 dts <- lapply(mtd, function(i) rapid_date(xml2::read_xml(i)))
-scl <- lapply(mtd, function(i) unique(rapid_sf(xml2::read_xml(i))))
+scl <- lapply(mtd, function(i) round(unique(rapid_sf(xml2::read_xml(i))), 2L))
 
 ## digital elevation models per rapideye tile
 dem_utm <- raster("dem/dem_srtm_01_utm.tif")
@@ -66,9 +67,7 @@ dms <- lapply(unique(cid), function(h) {
     raster(nm)
   } else {
     b1 <- raster(fl)
-    dm <- resample(dem_utm, b1, filename = nm)
-    # gdalUtils::gdal_translate(attr(dm@file, "name"), nm, of = "KEA", 
-    #                           output_Raster = TRUE)
+    resample(dem_utm, b1, filename = nm)
   }
 }); names(dms) <- unique(cid)
 
@@ -86,29 +85,38 @@ for (h in 1:length(mtd)) {
   ## digital elevation model
   dem <- dms[[grep(cid[h], names(dms))]]
 
-  ## aerosol optical depth (aod; which modis overpass is closer to rapideye?)
-  mod <- list.files("modis", full.names = TRUE, 
-                    pattern = "Aerosol_Optical_Depth.tif$")
-  mod <- mod[grep(format(dts[[h]], "%Y%j"), mod)]
+  ## aerosol optical depth (aod; which modis overpass is closer to rapideye?), 
+  ## atmospheric water (wct) and ozone content (oct)
+  prj <- projectExtent(dem, crs = "+init=epsg:4326")
+  atm <- sapply(c("Aerosol_Optical_Depth", "Water_Vapor", "Total_Ozone"), 
+                function(z) {
+    
+    mod <- list.files("modis", full.names = TRUE, pattern = paste0(z, ".tif$"), 
+                      recursive = TRUE)
+    mod <- mod[grep(format(dts[[h]], "%Y%j"), mod)]
   
-  aod <- do.call(mean, lapply(mod, function(fl) {
-    rst <- raster(fl)
-    crp <- crop(rst, projectExtent(dem, crs = "+init=epsg:4326"))
-    round(mean(crp[], na.rm = TRUE), digits = 3)
-  }))
+    val <- mean(sapply(mod, function(i) {
+      weightedAverage(i, prj, snap = "out")
+    }), na.rm = TRUE)
+    
+    if (z == "Total_Ozone") val <- val / 1000
+    return(round(val, digits = 2L))
   
-  aod <- aod_vld[which.min(abs(aod - aod_vld))]
+    # aod <- if (is.na(aod)) .01 else aod_vld[which.min(abs(aod - aod_vld))]
+    # wct <- if (is.na(wct)) .01 else wct_vld[which.min(abs(wct - wct_vld))]
+  })
   
   ## apply atmospheric correction
   cmd <- paste("arcsi.py -s rapideye -f KEA -p TOA SREF", 
                "--aeropro Continental --atmospro Tropical", 
-               "--aot", aod, "--dem", attr(dem@file, "name"), 
-               "--scalefac", scl[[h]], "-i", mtd[h],
+               "--atmoswater", atm[2], "--atmosozone", atm[3],
+               "--aot", atm[1], "--dem", attr(dem@file, "name"), 
+               "--scalefac", 1 / scl[[h]], "-i", mtd[h],
                "-t arcsidata/Temp -o arcsidata/Outputs")
   
-  if (h == 1) {
-    cat("#!/bin/bash\n\n", file = "arcsibatch.sh")
-  }
-  
-  cat(paste0(cmd, "\n\n"), file = "arcsibatch.sh", append = TRUE)
+  # if (h == 1) {
+  #   cat("#!/bin/bash\n\n", file = "arcsibatch.sh")
+  # }
+  # 
+  # cat(paste0(cmd, "\n\n"), file = "arcsibatch.sh", append = TRUE)
 }
