1791f5e0929be0d225fc3a59f59a5a5f56fbbb70
fdetsch <florian.detsch@staff.uni-marburg.de>
2017-07-06 15:15:26 +0200

    Minor edits.

--- R/ndvi.R
+++ R/ndvi.R
@@ -1,16 +1,18 @@
-setwd("/media/fdetsch/data/bale")
+setwd("/media/fdetsch/XChange/bale")
 
 library(MODIS)
-MODISoptions("/media/fdetsch/data/bale/MODIS_ARC", 
-             "/media/fdetsch/data/bale/MODIS_ARC/PROCESSED", 
+MODISoptions("/media/fdetsch/XChange/MODIS_ARC", 
+             "/media/fdetsch/XChange/MODIS_ARC/PROCESSED", 
              outProj = "+init=epsg:32637", MODISserverOrder = c("LPDAAC", "LAADS"))
 
-dem <- raster("dem/dem_srtm_01_utm_3742218.tif")
-dem <- as(extent(dem), "SpatialPolygons"); proj4string(dem) <- "+init=epsg:32637"
+# dem <- raster("dem/dem_srtm_01.tif")
+# dem <- as(extent(dem), "SpatialPolygons"); proj4string(dem) <- "+init=epsg:4326"
 
-tfs <- runGdal("M*D13Q1", collection = getCollection("MOD13Q1", forceCheck = TRUE), 
-               extent = dem, SDSstring = "101000000011", job = "balendvi", 
-               begin = "2013001", end = "2016366")
+stn <- shapefile("/media/permanent/data/bale/ema/ema_stations.shp")
+ext <- as(extent(stn), "SpatialPolygons"); proj4string(ext) <- proj4string(stn)
+bff <- rgeos::gBuffer(ext, width = 1e3)
 
-library(ESD)
-mcd <- preprocessMODIS(tfs, dsn = "ndvi", cores = 3L, lambda = 500L)
+tfs <- runGdal("M*D13A2", collection = getCollection("MOD13A2", forceCheck = TRUE), 
+               extent = bff, SDSstring = "101000000011", job = "balendvi", 
+               quiet = TRUE)
+
